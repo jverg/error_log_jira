2b5e5f09da384956577a1f9d8feab781dc13ea47
jverg <giannisverg@gmail.com>
2016-04-05 18:18:11 +0300

    Initial menu system.

--- src/Controller/AddForm.php
+++ src/Controller/AddForm.php
@@ -2,47 +2,76 @@
 
 namespace Drupal\error_log_jira\Controller;
 
-use Drupal\Core\Form\FormInterface;
+use Drupal\Core\Field\Plugin\Field\FieldType\StringItem;
+use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\RfcLogLevel;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
-class AddForm implements FormInterface {
+class AddForm extends FormBase {
 
   function getFormID() {
-    return 'error_log_jira_add';
+    return 'error_log_jira_list';
   }
 
-  function buildForm(array $form, FormStateInterface $form_state) {
-    $form['name'] = array(
-      '#type' => 'textfield',
-      '#title' => t('Name'),
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    // Form for the key for the jira project.
+    $form['key'] = array(
+      '#type'          => 'textfield',
+      '#title'         => t('jira Project KEY'),
+//      '#default_value' => variable_get('error_log_jira_key', ''),
+      '#description'   => t('Enter the KEY of your jira project.<br>
+                           (NOT THE TITLE).'),
+      '#required'      => TRUE,
+      '#size'          => 10,
     );
-    $form['message'] = array(
-      '#type' => 'textarea',
-      '#title' => t('Message'),
+// The select form.
+    $error_log_jira_options = array(
+      '0'  => 'No Values',
+      '1'  => '1',
+      '6'  => '6',
+      '10' => '10',
+      '25' => '25',
     );
-    $form['actions'] = array(
-      '#type' => 'actions'
+    $form['number_select'] = array(
+      '#type'          => 'select',
+      '#title'         => t('Number of items to be checked.'),
+      '#options'       => $error_log_jira_options,
+      '#description'   => t('Select how many values do you want<br> to be
+                             checked on every cron run.'),
+//      '#default_value' => variable_get('error_log_jira_number_select', '0'),
     );
-    $form['actions']['submit'] = array(
-      '#type' => 'submit',
-      '#value' => t('Add'),
+
+    // Checkboxes form.
+    $form['jira_severity_levels'] = array(
+      '#type'          => 'checkboxes',
+      '#title'         => t('Type of log messages'),
+      '#options'       => RfcLogLevel::getLevels(),
+      '#required'      => TRUE,
     );
+
+    // Submit form for the "Save" button.
+    $form['submit'] = array(
+      '#type'  => 'submit',
+      '#value' => t('Save'),
+    );
+
     return $form;
   }
 
-  function validateForm(array &$form, FormStateInterface $form_state) {
+  public function validateForm(array &$form, FormStateInterface $form_state) {
     /*
      * Nothing to validate on this form
      */
   }
 
-  function submitForm(array &$form, FormStateInterface $form_state) {
-    $name = $form_state['values']['name'];
-    $message = $form_state['values']['message'];
-    BdContactStorage::add(check_plain($name), check_plain($message));
-    watchdog('bd_contact', 'BD Contact message from %name has been submitted.', array('%name' => $name));
-    drupal_set_message(t('Your message has been submitted'));
-    $form_state['redirect'] = 'admin/content/bd_contact';
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $jira_key = $form_state['values']['key'];
+    $jira_number = $form_state['values']['number_select'];
+    $jira_severity_levels = $form_state['values']['jira_severity_levels'];
+    drupal_set_message(t('Your settings has been saved'));
+    $form_state['redirect'] = 'error_log_jira';
     return;
   }
 }
